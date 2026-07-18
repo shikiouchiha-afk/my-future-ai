6ee8580eb491f88b645c14b8abdafdb4f915ba3c
Raphael Banks <shikiouchiha@gmail.com>
2026-07-17 23:50:08 -0600

    fix CoachMemory type

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -8,7 +8,8 @@ import {
   buildMemorySummary,
   getCoachOpeningMessage,
   getCoachProfile,
-  type CoachMemory,
+ type CoachMemory,
+type CoachKey,
 } from "@/lib/coachSystem";
 import {
   loadCoachMemory,
